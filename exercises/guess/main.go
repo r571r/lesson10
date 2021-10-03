4ba0ee3d27dbc6e0552d32eafc76cc4fba8cf342
Jaroslavs Samcuks <yarcat@gmail.com>
2021-10-03 20:02:39 +0200

    added exercises/

---
+++ exercises/guess/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("The programs guesses a number.")
+	fmt.Print("Please think of a number in range [1,99] and press ENTER...")
+	fmt.Scanln()
+
+	// IMPLEMENT ME HERE !
+}
---
+++ exercises/sumdigits/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("The program finds the sum of digits of N.")
+	fmt.Print("Enter N: ")
+	var n uint
+	fmt.Scan(&n)
+	// IMPLEMENT ME HERE !
+}
